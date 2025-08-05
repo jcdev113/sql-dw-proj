311d5b264d66515c8cbee034d692b1849246278d
jcdev113 <jakechiu0113@gmail.com>
2025-08-05 13:09:25 +0800

    Update init_database.sql
    Error fixed

--- scripts/SQLServer/init_database.sql
+++ scripts/SQLServer/init_database.sql
@@ -3,12 +3,12 @@
 Create Database and Schema
 =================================================
 Script Purpose: 
-	This script creates a new database named 'DataWarehouse' affter checking if it is existing.
+  This script creates a new database named 'DataWarehouse' affter checking if it is existing.
   If the database exists it will be dropped and recreated. Additionally, the script sets up three schemas 
   within the database: 'bronze', 'silver', and 'gold'.
     
 WARNING:
-	Executing the script will drop the entire 'DataWarehouse' if exists. 
+  Executing the script will drop the entire 'DataWarehouse' if exists. 
   All the data previously in the db will be permanently removed. 
   Please proceed with caution and ensure full backups before running the script.
 */
@@ -16,16 +16,18 @@ WARNING:
 USE master;
 GO
 
--- Drop the db if it exists
+-- Drop the database if it exists
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
-	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE DataWarehouse;
+  ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE DataWarehouse;
 END;
 GO
+-- DROP DATABASE IF EXISTS DataWarehouse;
 
 -- Create 'DataWarehouse' db
 CREATE DATABASE DataWarehouse;
+GO
 
 USE DataWarehouse;
 GO
