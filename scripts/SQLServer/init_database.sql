90957ce1786d284d7aca5a57c835313d46b7a317
jcdev113 <jakechiu0113@gmail.com>
2025-08-05 12:05:57 +0800

    Update init_database.sql
    fix typo

--- scripts/SQLServer/init_database.sql
+++ scripts/SQLServer/init_database.sql
@@ -20,7 +20,7 @@ GO
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
 	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE DataWarehous;
+	DROP DATABASE DataWarehouse;
 END;
 GO
 
